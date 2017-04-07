deb1ca81d46b1bbf99aa3578533a558722e92e3e
LeonBogod <combatstriker111@gmail.com>
2017-04-07 17:07:39 +0300

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,34 +1,17 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var words = [
-    ["E", "L", "E", "C","T","R","O","N","I","C","S"],
+    ["E", "L", "E", "C", "T", "R", "O", "N", "I", "C", "S"],
     ["A", "R", "R", "A", "Y"],
-    ["E","A","S","T","E","R"],
-    ["A","M","E","R","I","C","A"],
-    ["A","P","P","L","E"]
+    ["E", "A", "S", "T", "E", "R"],
+    ["A", "M", "E", "R", "I", "C", "A"],
+    ["A", "P", "P", "L", "E"],
+    ["I","T","A","L","Y"]
 ]
 
 
-var hints=["this is something programmers, designers and many other people use","This is tech related","this is a country" ," this is a fruit"]
+var hints = ["this is something programmers, designers and many other people use", "This is tech related", "this is a country", " this is a fruit"]
 
 
- var wongame=false;
+var wongame = false;
 
 var imagevalue = 0;
 var randomword = words[Math.floor(Math.random() * words.length)];
@@ -40,29 +23,28 @@ var tries = 7;
 
 
 
-function solveword(){
+function solveword() {
 
-//function to check if the users word is equal to the user word
-var g=prompt("enter full word  if you dare");
+    //function to check if the users word is equal to the user word
+    var g = prompt("enter full word  if you dare");
 
 
- var word=randomword.join("");
-//if user input uppercase equals the word then wongame is true
-if(g.toUpperCase()===word){
-  wongame=true;
-  alert("Congratulations!! you won!!!!")
-  sortingComplete();
-fireworks();
+    var word = randomword.join("");
+    //if user input uppercase equals the word then wongame is true
+    if (g.toUpperCase() === word) {
+        wongame = true;
+        alert("Congratulations!! you won!!!!")
+        sortingComplete();
+        fireworks();
 
-}
-else{
+    } else {
 
-  alert("sorry you loose")
-document.getElementById("win").innerHTML="you have been hanged!!"+" "+"the word was"+" "+randomword.join("");
-document.getElementById('hangman').src="https://lh3.ggpht.com/RHKkf_YzgC24WVjefBEHCVwZ1alzcA15oQgfsFd0tcgQo3_fgxFmLbyIGECcrZBD4KsJ=w300";
-document.getElementById("check").disabled = true;
-document.getElementById("hint").disabled = true;
-}
+        alert("sorry you loose")
+        document.getElementById("win").innerHTML = "you have been hanged!!" + " " + "the word was" + " " + randomword.join("");
+        document.getElementById('hangman').src = "https://lh3.ggpht.com/RHKkf_YzgC24WVjefBEHCVwZ1alzcA15oQgfsFd0tcgQo3_fgxFmLbyIGECcrZBD4KsJ=w300";
+        document.getElementById("check").disabled = true;
+        document.getElementById("hint").disabled = true;
+    }
 
 
 }
@@ -72,14 +54,14 @@ for (var i = 0; i < newword.length; i++) {
 }
 
 function printword() {
-//function to print blanks equal to the word length
+    //function to print blanks equal to the word length
 
 
     for (var i = 0; i < newword.length; i++) {
 
         var wor = document.getElementById('word');
         var tuna = document.createTextNode(newword[i]);
-      
+
         wor.appendChild(tuna);
         console.log(tuna);
     }
@@ -93,17 +75,17 @@ function printword() {
 // Image factory
 var quizImagesB = new Array();
 
-quizImagesB[0]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H1.gif"
-quizImagesB[1]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H1.gif"
-quizImagesB[2]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H2.gif"
-quizImagesB[3]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H3.gif"
-quizImagesB[4]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H4.gif"
-quizImagesB[5]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H5.gif"
-quizImagesB[6]="http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H6.gif"
+quizImagesB[0] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H1.gif"
+quizImagesB[1] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H1.gif"
+quizImagesB[2] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H2.gif"
+quizImagesB[3] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H3.gif"
+quizImagesB[4] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H4.gif"
+quizImagesB[5] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H5.gif"
+quizImagesB[6] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H6.gif"
 
 // output
 function checkletter() {
-//function to check user value to letters of the word
+    //function to check user value to letters of the word
     var value = document.getElementById("guess").value;
 
     console.log(value);
@@ -125,7 +107,7 @@ function checkletter() {
     }
     var ratefeld = document.getElementById("word");
     ratefeld.innerHTML = "";
-    document.getElementById('cash').innerHTML="you have "+" "+money+" "+"gold";
+    document.getElementById('cash').innerHTML = "you have " + " " + money + " " + "gold";
 
     printword();
 
@@ -140,61 +122,58 @@ function checkletter() {
         tries--;
         wrong.appendChild(text);
         wrongletters.push(text);
-           update();
-         }
-
-         document.getElementById('lives').innerHTML="you have"+" "+tries+ " "+"tries "+"left";
-        var wongame = true;
-for (var i = 0; i < newword.length; i++){
-if(newword[i] === "_ "){
-wongame = false;
-}
-}
-
-if(wongame==true){
-alert("You win! Congratulations!!!!");
-		sortingComplete();
-fireworks();
-
-}
-
-//once you got six wrong letters, you lose
-if(tries === 0){
-document.getElementById('hangman').src="https://lh3.ggpht.com/RHKkf_YzgC24WVjefBEHCVwZ1alzcA15oQgfsFd0tcgQo3_fgxFmLbyIGECcrZBD4KsJ=w300";
-alert("Uh...I guess you're dead now. Game over!!");
-document.getElementById("win").innerHTML="you have been hanged!!"+" "+"the word was"+" "+randomword.join("");
- document.getElementById("check").disabled = true;
- document.getElementById("hint").disabled = true;
-}
-
-
-
-}
-
- function update(val){
-
-
-
-
-  imagevalue++;
-    var url = 'url(' + quizImagesB[i] + ')';
-    document.getElementById('hangman').src=quizImagesB[imagevalue];
-
-
-    if (tries===0){
-          document.getElementById('hangman').src="https://lh3.ggpht.com/RHKkf_YzgC24WVjefBEHCVwZ1alzcA15oQgfsFd0tcgQo3_fgxFmLbyIGECcrZBD4KsJ=w300";
-
-       
+        update();
     }
-  };
-      
-    
+
+    document.getElementById('lives').innerHTML = "you have" + " " + tries + " " + "tries " + "left";
+    var wongame = true;
+    for (var i = 0; i < newword.length; i++) {
+        if (newword[i] === "_ ") {
+            wongame = false;
+        }
+    }
+
+    if (wongame == true) {
+        alert("You win! Congratulations!!!!");
+        sortingComplete();
+        fireworks();
+
+    }
+
+    //once you got six wrong letters, you lose
+    if (tries === 0) {
+        document.getElementById('hangman').src = "https://lh3.ggpht.com/RHKkf_YzgC24WVjefBEHCVwZ1alzcA15oQgfsFd0tcgQo3_fgxFmLbyIGECcrZBD4KsJ=w300";
+        alert("Uh...I guess you're dead now. Game over!!");
+        document.getElementById("win").innerHTML = "you have been hanged!!" + " " + "the word was" + " " + randomword.join("");
+        document.getElementById("check").disabled = true;
+        document.getElementById("hint").disabled = true;
+    }
+
+
+
+}
+
+function update(val) {
+
+
+
+
+    imagevalue++;
+    var url = 'url(' + quizImagesB[i] + ')';
+    document.getElementById('hangman').src = quizImagesB[imagevalue];
+
+
+    if (tries === 0) {
+        document.getElementById('hangman').src = "https://lh3.ggpht.com/RHKkf_YzgC24WVjefBEHCVwZ1alzcA15oQgfsFd0tcgQo3_fgxFmLbyIGECcrZBD4KsJ=w300";
+
+
+    }
+};
+
+
 document.addEventListener("DOMContentLoaded", function() {
     printword();
 });
-
-
-
 
 
 
